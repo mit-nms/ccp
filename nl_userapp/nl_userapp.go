d4ca157ed7320a0f27dc70d9251d125b795882d0
Akshay Narayan <akshayn@mit.edu>
2017-04-19 14:08:29 -0400

    nl_userapp: test kernel communication with ccp messages

--- nl_userapp/nl_userapp.go
+++ nl_userapp/nl_userapp.go
@@ -3,38 +3,96 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
-	"golang.org/x/sys/unix"
+	"time"
 
-	"github.com/mdlayher/netlink"
+	"ccp/ipcBackend"
+	"ccp/netlinkipc"
+
+	log "github.com/Sirupsen/logrus"
 )
 
 func main() {
-	nl, err := netlink.Dial(unix.NETLINK_USERSOCK, &netlink.Config{Groups: 0})
+	nl, err := netlinkipc.New().SetupSend("", 33).SetupListen("", 33).SetupFinish()
 	if err != nil {
-		fmt.Println(err)
+		log.WithFields(log.Fields{
+			"where": "recv setup",
+		}).Warn(err)
 		return
 	}
 
-	nl.JoinGroup(uint32(22))
+	msgs := nl.Listen()
+	msg := <-msgs
 
-	fmt.Println("receiving...")
-	msgs, err := nl.Receive()
+	log.WithFields(log.Fields{
+		"msg": msg,
+	}).Info("got msg")
+	err = expectCreate(msg, 42, "reno")
 	if err != nil {
-		fmt.Println(err)
-		return
+
 	}
 
-	for _, msg := range msgs {
-		fmt.Printf("got message: %s\n", string(msg.Data))
+	cwMsg := nl.GetCwndMsg()
+	cwMsg.New(42, 4380)
+	nl.SendMsg(cwMsg)
+
+	msg = <-msgs
+
+	log.WithFields(log.Fields{
+		"msg": msg,
+	}).Info("got msg")
+	err = expectAck(msg, 42, 1461, time.Duration(255)*time.Nanosecond)
+}
+
+func expectCreate(msg ipcbackend.Msg, sid uint32, alg string) error {
+	switch msg.(type) {
+	case ipcbackend.CreateMsg:
+		cr := msg.(ipcbackend.CreateMsg)
+		if cr.SocketId() == sid && bytes.Equal([]byte(cr.CongAlg()), []byte(alg)) {
+			log.WithFields(log.Fields{
+				"got": fmt.Sprintf("(%v, %v)", cr.SocketId(), cr.CongAlg()),
+			}).Info("ok")
+		} else {
+			log.WithFields(log.Fields{
+				"expected":     fmt.Sprintf("(%v, %v)", sid, alg),
+				"got":          fmt.Sprintf("(%v, %v)", cr.SocketId(), cr.CongAlg()),
+				"alg bytes":    []byte(cr.CongAlg()),
+				"expect bytes": []byte(alg),
+			}).Warn("bad message")
+			return fmt.Errorf("bad message")
+		}
+	default:
+		log.WithFields(log.Fields{
+			"expected": fmt.Sprintf("(%v, %v)", sid, alg),
+		}).Warn("bad message type")
+		return fmt.Errorf("bad message type")
 	}
 
-	resp := netlink.Message{
-		Header: netlink.Header{
-			Flags: netlink.HeaderFlagsRequest | netlink.HeaderFlagsAcknowledge,
-		},
-		Data: []byte("response from go userspace"),
+	return nil
+}
+
+func expectAck(msg ipcbackend.Msg, sid uint32, ackNo uint32, rtt time.Duration) error {
+	switch msg.(type) {
+	case ipcbackend.AckMsg:
+		ack := msg.(ipcbackend.AckMsg)
+		if ack.SocketId() == sid && ack.AckNo() == ackNo {
+			log.WithFields(log.Fields{
+				"got": fmt.Sprintf("(%v, %v, %v)", ack.SocketId(), ack.AckNo(), ack.Rtt()),
+			}).Info("ok")
+		} else {
+			log.WithFields(log.Fields{
+				"expected": fmt.Sprintf("(%v, %v, %v)", sid, ackNo, rtt),
+				"got":      fmt.Sprintf("(%v, %v, %v)", ack.SocketId(), ack.AckNo(), ack.Rtt()),
+			}).Warn("bad message")
+			return fmt.Errorf("bad message")
+		}
+	default:
+		log.WithFields(log.Fields{
+			"expected": fmt.Sprintf("(%v, %v, %v)", sid, ackNo, rtt),
+		}).Warn("bad message type")
+		return fmt.Errorf("bad message type")
 	}
 
-	nl.Send(resp)
+	return nil
 }
